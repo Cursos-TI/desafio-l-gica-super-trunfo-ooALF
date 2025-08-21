78810b7f3d2bd7f435b6412fc62046075f32446e
João Victor  Costa <jbezerra30@hotmail.com>
2025-08-21 13:35:15 +0000

    jogo concluido

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,10 +6,13 @@ int main (){
 
 char cidade[20], cidade2[20]; //nome da cidade
 char estado[20], estado2[20]; //primeira letra do estado
+char criterio [20], criterio2[20]; // critério de comparação
 char codigo[20],codigo2[20];  // codigo da carta (EX A01, A02 )
 int populacao, populacao2;  // população da cidade
+int valor1, valor2, valor3, valor4;
 int turistico, turistico2; // pontos turisticos da cidade
-int batalha;
+int batalha, batalha2; // variavel para batalha
+int resultado, resultado2; // resultado da batalha
 float area,area2; // tamanho da cidade
 float pib, pib2;  // pib
 float divisao, divisao2;  // pib per capita
@@ -20,11 +23,11 @@ float superpoder, superpoder2; // poder da carta
 // dados carta 1 do jogo
 
 printf ("Primeira letra do Estado:\n");
-scanf ("%s",&estado);
+scanf ("%s",estado);
 printf("Codigo da carta de 1 à 4 (ex A01, A03):\n");
 scanf("%s",&codigo);
 printf ("Nome da Cidade:\n");
-scanf ("%s",&cidade);
+scanf ("%s",cidade);
 printf ("População da Cidade:\n");
 scanf ("%d",&populacao);
 printf ("Pontos Turisticos:\n");
@@ -39,11 +42,11 @@ scanf ("%f",&pib);
 // dados carta do 2 do jogo
 
 printf ("Primeira letra do Estado:\n");
-scanf ("%s",&estado2);
+scanf ("%s",estado2);
 printf("Codigo da carta de 1 à 4 (ex A01, A03):\n");
 scanf("%s",&codigo2);
 printf ("Nome da Cidade:\n");
-scanf ("%s",&cidade2);
+scanf ("%s",cidade2);
 printf ("População da Cidade:\n");
 scanf ("%d",&populacao2);
 printf ("Pontos Turisticos:\n");
@@ -65,8 +68,8 @@ areadiv2 = ( populacao2 + area2) / 2;
 
 
 // calculo carta 1 e 2 densidade inverso 
-deninverso = 1/areadiv;
-deninverso2 = 1/areadiv2;
+deninverso = 1.0/areadiv;
+deninverso2 = 1.0/areadiv2;
 
 //calculo super poder
 superpoder = populacao + area + pib + turistico + divisao + deninverso;
@@ -106,7 +109,7 @@ printf("\n");
 
 //Batalha
 printf ("=================================\n");
-printf ("            BATALHA              \n");
+printf ("             ROUND 1             \n");
 printf ("=================================\n");
 printf ("\n");
 
@@ -118,109 +121,191 @@ printf ("4. Pontos turisticos\n");
 printf ("5. Densidade demográfica\n");
 scanf ("%d",&batalha);
 
-//resultado da batalha
-printf ("=================================\n");
-printf ("           RESULTADO             \n");
-printf ("=================================\n");
+
 
 switch (batalha)
 {
-case 1:
-   if (populacao > populacao2){
-
-      printf ("%s venceu!!!\n",cidade);
-      printf ("Metodo de comparação foi População\n");
-      printf ("%s Contra %s\n",cidade,cidade2);
-      printf ("A População de %s foi %d \n A População de %s foi %d",cidade,populacao,cidade2,populacao2);
-   } 
-
-   else if (populacao < populacao2){
-      printf ("%s venceu!!!",cidade2);
-      printf ("Metodo de comparação foi População");
-      printf ("%s Contra %s",cidade,cidade2);
-      printf ("A População de %s foi %d \n A População de %s foi %d\n",cidade2,populacao2,cidade,populacao);
-   } else {printf("Tivemos um empate\n");}
-   
-
-   
-   
+   case 1:
+    resultado = populacao > populacao2 ? 1 : 0;
+    sprintf(criterio, "População");
+    valor1 = populacao;
+    valor2 = populacao2;
    break;
+
 
    case 2:
-
-   if (area > area2){
-
-      printf ("%s venceu!!!\n",cidade);
-      printf ("Metodo de comparação foi Area\n");
-      printf ("%s Contra %s\n",cidade,cidade2);
-      printf ("A area de %s foi %f \n A area de %s foi %f\n",cidade,area,cidade2,area2);
-   } 
-
-   else if (area < area2){
-      printf ("%s venceu!!!\n",cidade2);
-      printf ("Metodo de comparação foi Area\n");
-      printf ("%s Contra %s\n",cidade2 ,cidade);
-      printf ("A Area de %s foi %f \n A Area de %s foi %f\n",cidade2,area2,cidade,area);
-   } else {printf("Tivemos um empate\n");}
+     resultado = area > area2 ? 1 : 0;
+     sprintf(criterio,"Area");
+     valor1 = area;
+     valor2 = area2;
    break;
+   
 
    case 3:
-
-   if (pib > pib2){
-
-      printf ("%s venceu!!!\n",cidade);
-      printf ("Metodo de comparação foi PIB\n");
-      printf ("%s Contra %s\n",cidade,cidade2);
-      printf ("O PIB de %s foi %f \n O PIB de %s foi %f\n",cidade,pib,cidade2,pib2);
-   } 
-
-   else if (pib < pib2){
-      printf ("%s venceu!!!\n",cidade2);
-      printf ("Metodo de comparação foi PIB\n");
-      printf ("%s Contra %s\n",cidade2 ,cidade);
-      printf ("O PIB de %s foi %f \n O PIB de %s foi %f\n",cidade2,pib2,cidade,pib);
-   } else {printf("Tivemos um empate\n");}
-
+    resultado = pib > pib2 ? 1 : 0;
+    sprintf(criterio,"PIB");
+     valor1 = pib;
+     valor2 = pib2;
    break;
+
 
    case 4:
-  if (turistico > turistico2){
-
-      printf ("%s venceu!!!\n",cidade);
-      printf ("Metodo de comparação foi Pontos turisticos\n");
-      printf ("%s Contra %s\n",cidade,cidade2);
-      printf ("Os pontos turisticos de %s foi %d \n Os pontos turisticos de %s foi %d\n",cidade,turistico,cidade2,turistico2);
-   } 
-
-   else if (turistico < turistico2){
-      printf ("%s venceu!!!\n",cidade2);
-      printf ("Metodo de comparação foi Pontos turisticos\n");
-      printf ("%s Contra %s\n",cidade2 ,cidade);
-      printf ("Os pontos turisticos %s foi %d\n Os pontos turisticos %s foi %d\n",cidade2,turistico2,cidade,turistico);
-   } else {printf("Tivemos um empate\n");}
+    resultado = turistico > turistico2 ? 1 : 0 ;
+    sprintf(criterio,"Pontos turisticos");
+   valor1 = turistico;
+   valor2 = turistico2;
    break;
-
+  
 
    case 5:
-   if (deninverso < deninverso2){
-
-      printf ("%s venceu!!!\n",cidade);
-      printf ("Metodo de comparação foi Pontos turisticos\n");
-      printf ("%s Contra %s\n",cidade,cidade2);
-      printf ("A Densidade Demográfica de %s foi %f \n A Densidade Demográfica de %s foi %f\n",cidade,deninverso,cidade2,deninverso2);
-   } 
-
-   else if (deninverso > deninverso2){
-      printf ("%s venceu!!!\n",cidade2);
-      printf ("Metodo de comparação foi Pontos turisticos\n");
-      printf ("%s Contra %s\n",cidade2 ,cidade);
-      printf ("A Densidade Demográfica de %s foi %f\n A Densidade Demográfica de %s foi %f\n",cidade2,deninverso2,cidade,deninverso);
-   } else {printf("Tivemos um empate\n");}
+     resultado = deninverso < deninverso2 ? 1 : 0;
+     sprintf(criterio,"Densidade demográfica");
+     valor1 = deninverso;
+     valor2 = deninverso2;
    break;
+
 
 default: ("  Opção invalida.\n");
-   break;
+   break;   
 }
 
+
+
+//Batalha 2
+printf ("=================================\n");
+printf ("             ROUND 2             \n");
+printf ("=================================\n");
+printf ("\n");
+
+printf ("Escolha o metodo de comparação\n");
+printf ("1. População\n");
+printf ("2. Área\n");
+printf ("3. PIB\n");
+printf ("4. Pontos turisticos\n");
+printf ("5. Densidade demográfica\n");
+scanf ("%d",&batalha2);
+
+
+if (batalha == batalha2)
+{
+   printf ("VOCÊ ESCOLHEU O MESMO METODO DE COMPARAÇÃO!!!!");
+   
+}  else { switch (batalha2)
+{
+   case 1:
+     resultado2 = populacao > populacao2 ? 1 : 0 ;
+     sprintf(criterio2,"População");
+     valor3 = populacao;
+     valor4 = populacao2;
+   break;
+
+
+   case 2:
+     resultado2 = area > area2 ? 1 : 0;
+     sprintf(criterio2,"Area");
+     valor3 = area;
+     valor4 = area2;
+   break;
+   
+
+   case 3:
+    resultado2 = pib > pib2 ? 1 : 0;
+    sprintf(criterio2,"PIB");
+    valor3 = pib;
+    valor4 = pib2;
+   break;
+
+
+   case 4:
+    resultado2 = turistico > turistico2 ? 1 : 0 ;
+    sprintf(criterio2,"Pontos turisticos");
+    valor3 = turistico;
+    valor4 = turistico2;
+   break;
+  
+
+   case 5:
+     resultado2 = deninverso < deninverso2 ? 1 : 0;
+     sprintf(criterio2,"Densidade demográfica");
+      valor3 = deninverso; 
+      valor4 = deninverso2;
+   break;
+
+
+default: ("  Opção invalida.\n");
+   break;   
+}
+}
+
+
+
+
+//resultado da batalha
+printf ("=================================\n");
+printf ("           RESULTADO  1          \n");
+printf ("=================================\n");
+
+ // RESULTADO DO PRIMEIRO CONFRONTO
+
+ if (resultado == 1) {
+ 
+   printf ("%s Venceu!!!\n",cidade);
+   printf ("Metodo de comparação foi %s\n",criterio);
+   printf ("%s Contra %s\n",cidade,cidade2);
+   printf ("%s de %s foi %.2f \n Os %s de %s foi %d\n",criterio, cidade, valor1, criterio, cidade2, valor2);
+  
+ } else if (resultado == 0){
+
+   printf ("%s Venceu!!!\n",cidade2);
+   printf ("Metodo de comparação foi %s\n",criterio);
+   printf ("%s Contra %s\n",cidade2,cidade);
+   printf ("%s de %s foi %.2f \n Os %s de %s foi %d\n",criterio, cidade, valor1, criterio, cidade2, valor2);
+
+ } else { printf ("EMPATE!!!\n");}
+ 
+printf ("=================================\n");
+printf ("           RESULTADO  2          \n");
+printf ("=================================\n");
+
+
+
+if (resultado2 == 1) {
+
+   printf ("%s Venceu!!!\n",cidade);
+   printf ("Metodo de comparação foi %s\n",criterio2);
+   printf ("%s Contra %s\n",cidade,cidade2);
+   printf ("%s de %s foi %.2f \n Os %s de %s foi %d\n",criterio2, cidade, valor3, criterio2, cidade2, valor4);
+  
+ } else if (resultado2 == 0)
+ {
+   printf ("%s Venceu!!!\n",cidade2);
+   printf ("Metodo de comparação foi %s\n",criterio2);
+   printf ("%s Contra %s\n",cidade,cidade2);
+   printf ("%s de %s foi %.2f \n Os %s de %s foi %d\n",criterio2, cidade, valor3, criterio2, cidade2, valor4);
+ } else {
+   printf ("EMPATE!!!\n");
+ }
+ 
+
+if ((resultado == 1 && resultado2 == 0) || (resultado == 0 && resultado2 == 1)) {
+   
+   printf("=================================\n");
+   printf("        EMPATE GERAL!            \n");
+   printf("   Cada carta venceu um round.   \n"); 
+   printf("=================================\n");
+
+} else if (resultado == 1 && resultado2 == 1) {
+
+    printf("=================================\n");
+    printf("   %s VENCEU OS DOIS ROUNDS!   \n", cidade);
+    printf("=================================\n");
+
+} else if (resultado == 0 && resultado2 == 0) {
+
+    printf("=================================\n");
+    printf("   %s VENCEU OS DOIS ROUNDS!   \n", cidade2);
+    printf("=================================\n");
+  
+}
 return 0;
 }
